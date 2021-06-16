539426742bcd41b92fd4c102e51005831ed6c283
unknown <jayaram.l@mobileprogrammingllc.com>
2021-06-16 11:17:53 +0530

    sniper ported

--- snipe_port.js
+++ snipe_port.js
@@ -17,6 +17,7 @@
 
 //unsafeWindow.on = true
 //axios.defaults.adapter = axiosGmxhrAdapter
+
 const newToken = unsafeWindow.Game.csrfToken
 
 const TOWN_MAP = new Map()
@@ -31,24 +32,34 @@ TOWN_MAP["Titan 8"] = '4322'
 TOWN_MAP["Titan 9"] = '9984' // => bireme
 TOWN_MAP["Titan 10"] = '10610' // => land def
 
+
+const BOOST_MAP = new Map()
+BOOST_MAP["ls"] = { wood: 29500, stone: 6500, iron: 18600 }
+BOOST_MAP["bir"] = { wood: 29500, stone: 7482, iron: 0 }
+BOOST_MAP["hop"] = { wood: 0, stone: 15000, iron: 30000 }
+BOOST_MAP["sling"] = { wood: 16500, stone: 29500, iron: 12000 }
+BOOST_MAP["horse"] = { wood: 20000, stone: 10000, iron: 29500 }
+BOOST_MAP["dlu"] = { wood: 29500, stone: 0, iron: 27000 }
+BOOST_MAP["all"] = { wood: 29000, stone: 29000, iron: 29000 }
+
 const sameIslandDLU = [
     {
-        cs_time: moment.tz("2021-06-10 11:05:03", "Europe/Madrid").unix() - 1,
+        cs_time: moment.tz("2021-06-15 08:33:25", "Europe/Madrid").unix() - 1,
         anchors: [
-            '{ "sword": 23, "archer": 31, "hoplite": 1, "rider": 4, "chariot": 1, "catapult": 1, "id": 9070, "type": "support", "town_id": ' + TOWN_MAP['Titan'] + ', "nl_init": true }',
-            '{ "sword": 23, "archer": 31, "hoplite": 1, "rider": 4, "chariot": 1, "id": 9070, "type": "support", "town_id": ' + TOWN_MAP['Titan'] + ', "nl_init": true }'
+            '{ "hoplite": 157, "rider": 53, "catapult": 1, "id": 9367, "type": "support", "town_id": ' + TOWN_MAP['Titan 3'] + ', "nl_init": true }',
+            '{ "hoplite": 157, "rider": 53, "id": 9367, "type": "support", "town_id": ' + TOWN_MAP['Titan 3'] + ', "nl_init": true }'
         ]
     }
 ]
 
 const diffIslandDLU1 = [
     {
-        cs_time: moment.tz("2021-06-10 23:09:00", "Europe/Madrid").unix() - 1,
+        cs_time: moment.tz("2021-06-15 15:38:55", "Europe/Madrid").unix() - 1,
         anchors: [
-            '{"hoplite": 110, "rider": 54, "small_transporter": 1, "demolition_ship": 1, "id": 9070, "type": "support", "town_id": ' + TOWN_MAP['Titan'] + ', "nl_init": true }',
-            '{"sword": 23, "archer": 31, "hoplite": 1, "rider": 4, "chariot": 1, "small_transporter": 1, "big_transporter": 1, "id": 9070, "type": "support", "town_id": ' + TOWN_MAP['Titan'] + ', "nl_init": true }',
-            '{"hoplite": 110, "rider": 54, "small_transporter": 17, "attack_ship": 1, "id": 5105, "type": "support", "town_id": ' + TOWN_MAP['Titan 3'] + ', "nl_init": true }',
-            '{"hoplite": 110, "rider": 54, "small_transporter": 17, "id": 5105, "type": "support", "town_id": ' + TOWN_MAP['Titan 3'] + ', "nl_init": true }'
+            //'{"hoplite": 157, "rider": 53, "small_transporter": 20, "demolition_ship": 1, "id": 5105, "type": "support", "town_id": ' + TOWN_MAP['Titan 3'] + ', "nl_init": true }',
+            '{"hoplite": 157, "rider": 53, "small_transporter": 20, "big_transporter": 1, "id": 5105, "type": "support", "town_id": ' + TOWN_MAP['Titan 3'] + ', "nl_init": true }',
+            '{"hoplite": 157, "rider": 53, "small_transporter": 20, "attack_ship": 1, "id": 5105, "type": "support", "town_id": ' + TOWN_MAP['Titan 3'] + ', "nl_init": true }',
+            '{"hoplite": 157, "rider": 53, "small_transporter": 20, "id": 5105, "type": "support", "town_id": ' + TOWN_MAP['Titan 3'] + ', "nl_init": true }'
         ]
     }
 ]
@@ -67,22 +78,17 @@ const diffIslandDLU2 = [
 
 const bireme = [
     {
-        cs_time: moment.tz("2021-06-11 10:21:00", "Europe/Madrid").unix() - 1,
+        cs_time: moment.tz("2021-06-15 15:38:55", "Europe/Madrid").unix() - 1,
         anchors: [
-            '{ "bireme": 26, "demolition_ship": 1, "id": 5105, "type": "support", "town_id": ' + TOWN_MAP['Titan 9'] + ', "nl_init": true }',
-            '{ "bireme": 26, "big_transporter": 1, "id": 5105, "type": "support", "town_id": ' + TOWN_MAP['Titan 9'] + ', "nl_init": true }',
-            '{ "bireme": 26, "attack_ship": 1, "id": 5105, "type": "support", "town_id": ' + TOWN_MAP['Titan 9'] + ', "nl_init": true }',
-            '{ "bireme": 26, "id": 5105, "type": "support", "town_id": ' + TOWN_MAP['Titan 9'] + ', "nl_init": true }'
+            '{ "bireme": 44, "demolition_ship": 1, "id": 5105, "type": "support", "town_id": ' + TOWN_MAP['Titan 9'] + ', "nl_init": true }',
+            '{ "bireme": 44, "big_transporter": 1, "id": 5105, "type": "support", "town_id": ' + TOWN_MAP['Titan 9'] + ', "nl_init": true }',
+            '{ "bireme": 44, "attack_ship": 1, "id": 5105, "type": "support", "town_id": ' + TOWN_MAP['Titan 9'] + ', "nl_init": true }',
+            '{ "bireme": 44, "id": 5105, "type": "support", "town_id": ' + TOWN_MAP['Titan 9'] + ', "nl_init": true }'
         ]
     }
 ]
 
 let interval = null
-
-
-
-
-interval = setInterval(() => autoFarm(newToken), (600000 + (Math.random(5) * 60000)))
 
 
 const cityBoosting = async (newToken, townToBoost, boostingStrategy) => {
@@ -92,7 +98,7 @@ const cityBoosting = async (newToken, townToBoost, boostingStrategy) => {
         let result = {
             data: await GM_Get(getTownsUrl)
         }
-        let towns = _.filter(result.data.json.towns, (town) => { return town.id !== parseInt(townToBoost) })
+        let towns = _.filter(result.data.json.towns, (town) => { return town.id !== parseInt(townToBoost) && town.id !== parseInt(TOWN_MAP["Titan 9"]) && town.id !== parseInt(TOWN_MAP["Titan 10"]) })
         return _.map(towns, (obj) => {
             return {
                 id: obj.id, res: obj.res
@@ -171,6 +177,9 @@ const cityBoosting = async (newToken, townToBoost, boostingStrategy) => {
                 console.error(result.data.json)
                 return
             }
+            let getPayload = encodeURI('{"town_id":' + townToBoost + ',"nl_init":true}')
+            let getURL = `https://es98.grepolis.com/game/town_overviews?town_id=${townToBoost}&action=trade_overview&h=${newToken}&json=${getPayload}&_=1623773044188`
+            await GM_Get(getURL)
             // let tempResult = _.find(result.data.json.notifications, { "subject": "Trade" })
             // let finalTradeData = JSON.parse(tempResult.param_str)
             // await sleep(finalTradeData.Trade.arrival_seconds_left * 1000)
@@ -188,10 +197,21 @@ const cityBoosting = async (newToken, townToBoost, boostingStrategy) => {
     }
 }
 
+
 const autoFarm = async (newToken) => {
-    let collect_url = `https://es98.grepolis.com/game/farm_town_overviews?town_id=9317&action=claim_loads_multiple&h=${newToken}`
-    let payload = '{"towns":[5989,10610,7461,9317,9378,9067,4322,9984,5974,9754],"time_option_base":300,"time_option_booty":600,"claim_factor":"normal","town_id":9317,"nl_init":true}'
+    //await sleep(time)
+    let collect_url = `https://es98.grepolis.com/game/farm_town_overviews?town_id=9067&action=claim_loads_multiple&h=${newToken}`
+    let payload = '{"towns":[5989,10610,7461,9317,9378,9067,4322,9984,5974,9754],"time_option_base":300,"time_option_booty":600,"claim_factor":"normal","town_id":9984,"nl_init":true}'
     await GM_Post(collect_url, 'json=' + encodeURI(payload))
+    //let getPayload1 = encodeURI('{"chunks":[{"x":29,"y":24,"timestamp":1623407194},{"x":28,"y":24,"timestamp":1623407194},{"x":29,"y":23,"timestamp":1623406964},{"x":28,"y":23,"timestamp":1623407194}],"town_id":9378,"nl_init":true}')
+    //let getUrl1 = `https://es98.grepolis.com/game/map_data?town_id=9378&action=get_chunks&h=${newToken}&json=${getPayload1}&_=1623403896854`
+    //let getPayload2 = encodeURI('{"island_x":582,"island_y":481,"current_town_id":9378,"booty_researched":1,"diplomacy_researched":"","trade_office":0,"town_id":9378,"nl_init":true}')
+    //let getUrl2 = `https://es98.grepolis.com/game/farm_town_overviews?town_id=9378&action=get_farm_towns_for_town&h=${newToken}&json=${getPayload2}&_=1623403896855`
+    //await GM_Get(getUrl1)
+    //await GM_Get(getUrl2)
+    let getPayload = encodeURI('{"town_id":9067,"nl_init":true}')
+    let getURL = `https://es98.grepolis.com/game/farm_town_overviews?town_id=9067&action=fetch&h=${newToken}&json=${getPayload}&_=1623420284183`
+    await GM_Get(getURL)
     console.log('Autofarm running')
 }
 
@@ -238,7 +258,7 @@ const startAttack = async (newToken, from, payload) => {
             else {
                 await sleep(1400)
             }
-            antiTimer(payload, cs_time)
+            await antiTimer(payload, cs_time)
         }
     }
 
@@ -281,13 +301,13 @@ const startAttack = async (newToken, from, payload) => {
     try {
         for (let i of payload) {
             for (let j of i.anchors) {
-                initialAttack(j, i.cs_time)
-                await sleep(2500)
+                await initialAttack(j, i.cs_time)
+                await sleep(5000)
             }
         }
     } catch (error) {
         console.error(error)
-        process.exit(0)
+        //process.exit(0)
     }
 
     // await antiTimer(slow1)
@@ -299,11 +319,29 @@ const sleep = (time) => {
     return new Promise((resolve) => setTimeout(resolve, time))
 }
 
+function getRandom(max) {
+    return Math.ceil(Math.random() * max);
+}
+
+
 function GM_Get(url) {
     return new Promise((resolve, reject) => {
         GM_xmlhttpRequest({
             method: "GET",
             url: url,
+            headers: {
+                "accept": "text/plain, */*; q=0.01",
+                "accept-language": "en-US,en;q=0.9",
+                "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-origin",
+                "sec-gpc": "1",
+                "x-requested-with": "XMLHttpRequest",
+                "origin": "origin: https://es98.grepolis.com",
+                "referrer": "https://es98.grepolis.com/game/index?login=1&p=848929475&ts=1623751483",
+                "cookie": document.cookie
+            },
             onload: function (response) {
                 resolve(JSON.parse(response.responseText))
             },
@@ -329,6 +367,8 @@ function GM_Post(url, payload) {
                 "sec-fetch-site": "same-origin",
                 "sec-gpc": "1",
                 "x-requested-with": "XMLHttpRequest",
+                "origin": "origin: https://es98.grepolis.com",
+                "referrer": "https://es98.grepolis.com/game/index?login=1&p=848929475&ts=1623751483",
                 "cookie": document.cookie
             },
             onload: function (response) {
@@ -343,4 +383,7 @@ function GM_Post(url, payload) {
 
 //startAttack(newToken, TOWN_MAP['Titan 10'], diffIslandDLU2)
 //startAttack(newToken, TOWN_MAP['Titan 3'], diffIslandDLU1)
-startAttack(newToken, TOWN_MAP['Titan 9'], bireme)
+//startAttack(newToken, TOWN_MAP['Titan 9'], bireme)
+//autoFarm(newToken)
+cityBoosting(newToken, TOWN_MAP["Titan 8"], BOOST_MAP["ls"])
+    //interval = setInterval(() => cityBoosting(newToken, TOWN_MAP["Titan 4"], BOOST_MAP["ls"]), 3600000)//</script>");
